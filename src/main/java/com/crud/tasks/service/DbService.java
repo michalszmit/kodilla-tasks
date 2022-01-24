52a1fb3e4f834f54255d6b31c5352d65fb6b7ae4
michalszmit <michalrd@gmail.com>
2022-01-24 17:28:03 +0100

    Modul 19.3 teoria

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +19,12 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Optional<Task> getTaskById(final Long id){
-        return repository.findById(id);
+    public Task getTask(final Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
     }
 
 }
